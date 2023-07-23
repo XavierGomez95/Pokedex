78c2a8d1857a41239000ac85a6462720ac0f4f2b
XavierGomez <xaviergomezxcv@gmail.com>
2023-07-23 23:16:27 +0200

    Generation buttons

--- src/components/ventana/PokedexPage.js
+++ src/components/ventana/PokedexPage.js
@@ -4,60 +4,60 @@ import PokemonCard from "../cards/PokemonCard";
 const PokedexPage = () => {
     const [pokemons, setPokemons] = useState([]);
     const [generation, setGeneration] = useState('1');
-    const [limit, setLimit] = useState(0);
 
 
     useEffect(() => {
-        handleGenerationChange(generation);
+        handleGenerationChange('1');
     }, []);
 
     const handleGenerationChange = async (selectedGeneration) => {
         setGeneration(selectedGeneration);
 
         try {
-            let offset;
+            let lastPokemonInPreviousGeneration;
+            let pokemonsInThisGeneration;
             switch (selectedGeneration) {
-                case '1':
-                    offset = 0;
-                    setLimit(151);
-                    break;
                 case '2':
-                    offset = 151;
-                    setLimit(100);
+                    lastPokemonInPreviousGeneration = 151;
+                    pokemonsInThisGeneration = 100;
                     break;
                 case '3':
-                    offset = 251;
-                    setLimit(135);
+                    lastPokemonInPreviousGeneration = 251;
+                    pokemonsInThisGeneration = 135;
                     break;
                 case '4':
-                    offset = 386;
-                    setLimit(107);
+                    lastPokemonInPreviousGeneration = 386;
+                    pokemonsInThisGeneration = 107;
                     break;
                 case '5':
-                    offset = 493;
-                    setLimit(156);
+                    lastPokemonInPreviousGeneration = 493;
+                    pokemonsInThisGeneration = 156;
                     break;
                 case '6':
-                    offset = 649;
-                    setLimit(72);
+                    lastPokemonInPreviousGeneration = 649;
+                    pokemonsInThisGeneration = 72;
                     break;
                 case '7':
-                    offset = 721;
-                    setLimit(88);
+                    lastPokemonInPreviousGeneration = 721;
+                    pokemonsInThisGeneration = 88;
                     break;
                 case '8':
-                    offset = 809;
-                    setLimit(96);
+                    lastPokemonInPreviousGeneration = 809;
+                    pokemonsInThisGeneration = 96;
                     break;
                 case '9':
-                    offset = 905;
-                    setLimit(105);
+                    lastPokemonInPreviousGeneration = 905;
+                    pokemonsInThisGeneration = 105;
+                    break;
+                default:
+                    lastPokemonInPreviousGeneration = 0;
+                    pokemonsInThisGeneration = 151;
                     break;
 
             }
 
             //limit=100000&offset=0.
-            const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=' + limit + '&offset=' + offset);
+            const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=' + pokemonsInThisGeneration + '&offset=' + lastPokemonInPreviousGeneration);
             if (!response.ok) {
                 console.error('Error al obtener los datos de los Pokémon.');
             } else {
@@ -81,18 +81,17 @@ const PokedexPage = () => {
         marginTop: 200,
         marginRight: 100,
         position: "sticky",
-        top: 200,
+        top: 120,
         height: "calc(100vh - 20vh)",
-        width: "calc(100vh - 85vh)",
-        overflowY: "auto",
+        width: "calc(100vh - 83vh)",
         padding: 30
     }
 
     return (
         <div className="container" >
             <div className="row">
-                <div className="col-1" style={margin}>
-                    <h4 className="mb-4">Generation</h4>
+                <div className="col-1" style={{...margin, overflowX: 'hidden'}}>
+                    <h4 className="mb-3">Generation</h4>
                         <button type="button"
                                 className={`btn btn-outline-info mb-3
                                 ${generation === '1' ? 'active' : ''}`}
